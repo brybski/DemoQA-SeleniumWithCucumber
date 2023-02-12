4d1e52271c73dc39fba6719773800a9bfa7a0b87
Bartlomiej Rybski <bartlomiej.rybski@gmail.com>
2023-02-13 00:13:03 +0100

    Adding some comments to TestContext class.

--- src/main/java/Utilities/TestContext.java
+++ src/main/java/Utilities/TestContext.java
@@ -3,6 +3,10 @@ package Utilities;
 import Managers.DriverManager;
 import Managers.PageObjectManager;
 
+/*
+TestContext is the parent class and the medium to share the information between the different steps in a test.
+Including PageObjectManager and DriverManager object to share between the steps.
+ */
 public class TestContext {
 
     private final DriverManager driverManager;
