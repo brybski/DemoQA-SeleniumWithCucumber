2d39b8217be8c569d63dbd083cbd89ec66672d4e
Bartlomiej Rybski <bartlomiej.rybski@gmail.com>
2023-02-21 23:36:38 +0100

    Update information about Test Context.

--- src/main/java/Utilities/TestContext.java
+++ src/main/java/Utilities/TestContext.java
@@ -6,6 +6,10 @@ import Managers.PageObjectManager;
 /*
 TestContext is the parent class and the medium to share the information between the different steps in a test.
 Including PageObjectManager and DriverManager object to share between the steps.
+For that purposes PicoContainer is used - Dependency Injection Container.
+PicoContainer really only has a single feature - it instantiates objects.
+Cucumber scans your classes with step definitions in them, passes them to PicoContainer,
+then asks it to create new instances for every scenario.
  */
 public class TestContext {
 
